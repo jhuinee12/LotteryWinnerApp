f00b8fb9b1d72f2d916589ac10ece7263e0d6ef2
jhuinee12 <jhuinee12@gmail.com>
2022-05-18 12:56:58 +0900

    회차선택 스피너 추가

--- app/src/main/java/com/zahi/lotto/presentation/winner/WinnerFragment.kt
+++ app/src/main/java/com/zahi/lotto/presentation/winner/WinnerFragment.kt
@@ -2,6 +2,9 @@ package com.zahi.lotto.presentation.winner
 
 import android.annotation.SuppressLint
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.lifecycle.ViewModelProvider
 import com.zahi.lotto.R
 import com.zahi.lotto.databinding.FragmentWinnerBinding
@@ -25,9 +28,24 @@ class WinnerFragment : BaseFragment<FragmentWinnerBinding>(R.layout.fragment_win
 
         binding.apply {
             dataViewModel = viewModel
-        }
 
-        viewModel.drwNo.value = latestDrwNo()
+            val latestDrwNo = latestDrwNo()
+            val drwNoArray = ArrayList<Long>()
+            for (i in latestDrwNo downTo 1) {
+                drwNoArray.add(i)
+            }
+
+            spinner.adapter = ArrayAdapter(requireContext(), R.layout.item_spinner, drwNoArray)
+            spinner.setSelection(0)
+            spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener {
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    viewModel.drwNo.value = spinner.selectedItem.toString().toLong()
+                }
+                override fun onNothingSelected(p0: AdapterView<*>?) { }
+                override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) { }
+            }
+
+        }
     }
 
     @SuppressLint("SetTextI18n")
