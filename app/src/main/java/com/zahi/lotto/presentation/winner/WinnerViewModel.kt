15c05ad62cb07d325c6d8fdb55970388ebdea960
jhuinee12 <jhuinee12@gmail.com>
2022-05-19 16:26:27 +0900

    PublishSubject를 이용하여 데이터 구독

--- app/src/main/java/com/zahi/lotto/presentation/winner/WinnerViewModel.kt
+++ app/src/main/java/com/zahi/lotto/presentation/winner/WinnerViewModel.kt
@@ -1,11 +1,9 @@
 package com.zahi.lotto.presentation.winner
 
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
 import com.zahi.lotto.entity.LotteryNumber
 import com.zahi.lotto.repositories.WinnerRepository
 import com.zahi.lotto.base.BaseViewModel
-import kotlinx.coroutines.launch
 
 
 class WinnerViewModel(private val winnerRepository: WinnerRepository) : BaseViewModel() {
@@ -13,7 +11,10 @@ class WinnerViewModel(private val winnerRepository: WinnerRepository) : BaseView
     var lotteryData: MutableLiveData<LotteryNumber> = MutableLiveData<LotteryNumber>()
 
     fun getLottoWinnerNumber() {
-        winnerRepository.getLottoWinnerNumber(drwNo.value!!, this@WinnerViewModel)
+        winnerRepository.subjectLotteryData.subscribe {
+            lotteryData.value = it
+        }
+        winnerRepository.getLottoWinnerNumber(drwNo.value!!)
     }
 
 }
--- app/src/main/java/com/zahi/lotto/repositories/WinnerRepository.kt
+++ app/src/main/java/com/zahi/lotto/repositories/WinnerRepository.kt
@@ -2,12 +2,15 @@ package com.zahi.lotto.repositories
 
 import android.util.Log
 import com.zahi.lotto.api.LottoApiService
-import com.zahi.lotto.presentation.winner.WinnerViewModel
+import com.zahi.lotto.entity.LotteryNumber
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.rxjava3.subjects.PublishSubject
 
 class WinnerRepository {
     private val lottoApi = LottoApiService.api
+
+    var subjectLotteryData: PublishSubject<LotteryNumber> = PublishSubject.create() // PublishSubject 객체 선언
 
     /**
      * public final Disposable subscribe(
@@ -15,13 +18,14 @@ class WinnerRepository {
      *      @NonNull Consumer<? super Throwable> onError,
      *      @NonNull Action onComplete)
      **/
-    fun getLottoWinnerNumber(drwNo: Long, viewModel: WinnerViewModel) = lottoApi.getLottoWinnerNumber(drwNo = drwNo)
+    fun getLottoWinnerNumber(drwNo: Long) = lottoApi.getLottoWinnerNumber(drwNo = drwNo)
             .subscribeOn(Schedulers.io())   // 생산자(Observable) 쪽의 데이터 흐름을 제어 :  I/O 처리 작업 진행 시 사용
             .observeOn(AndroidSchedulers.mainThread())  // 소비자(Observer) 쪽에서 전달받은 데이터 처리 제어 : 메인 쓰레드에서 진행
             .subscribe({    // 데이터 발행 및 소비
                 // onNext : 데이터의 발행을 알림
                 Log.d("getLottoWinnerNumber-Repository", "onNext: $it")
-                viewModel.lotteryData.value = it
+                subjectLotteryData.onNext(it)
+//                viewModel.lotteryData.value = it
             }, {
                 // onError : 오류가 발생했음을 알림
                 Log.d("getLottoWinnerNumber-Repository", "onError: $it")
