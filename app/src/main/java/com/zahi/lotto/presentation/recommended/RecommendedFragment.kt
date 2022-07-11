a218ba5727afe5253562890103e64a98b32fd433
jhuinee12 <jhuinee12@gmail.com>
2022-07-11 18:51:57 +0900

    [DEV-2] 로또 샘플앱 - 로또 추천 번호 6개 생성

--- app/src/main/java/com/zahi/lotto/presentation/recommended/RecommendedFragment.kt
+++ app/src/main/java/com/zahi/lotto/presentation/recommended/RecommendedFragment.kt
@@ -1,5 +1,6 @@
 package com.zahi.lotto.presentation.recommended
 
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import com.zahi.lotto.R
 import com.zahi.lotto.databinding.FragmentRecommendedBinding
@@ -11,15 +12,47 @@ class RecommendedFragment : BaseFragment<FragmentRecommendedBinding>(R.layout.fr
     private lateinit var viewModel: RecommendedViewModel
     private lateinit var viewModelFactory: RecommendedViewModelFactory
 
+    private val recommendedArray = arrayListOf<Int>()
+
     override fun initView() {
 
+        binding.apply {
+            this.btnRecommended.setOnClickListener {
+                for (i in 0..5) {
+                    while (true) {
+                        if (addRecommendedNumber()) break
+                    }
+                }
 
-        binding.apply { }
+                this.layoutRecommendedNumber.visibility = View.VISIBLE
+
+                recommendedArray.sort()
+
+                this.firstRecommendedNumber.text = recommendedArray[0].toString()
+                this.secondRecommendedNumber.text = recommendedArray[1].toString()
+                this.thirdRecommendedNumber.text = recommendedArray[2].toString()
+                this.fourthRecommendedNumber.text = recommendedArray[3].toString()
+                this.fifthRecommendedNumber.text = recommendedArray[4].toString()
+                this.sixthRecommendedNumber.text = recommendedArray[5].toString()
+            }
+        }
     }
 
     override fun initViewModel() {
         viewModelFactory = RecommendedViewModelFactory()
         viewModel = ViewModelProvider(this, viewModelFactory).get(RecommendedViewModel::class.java)
+    }
+
+    private fun createRandomNumber() = (1..45).random()
+
+    private fun addRecommendedNumber(): Boolean {
+        val random = createRandomNumber()
+        return if (!recommendedArray.contains(random)) {
+            recommendedArray.add(random)
+            true
+        } else {
+            false
+        }
     }
 
     companion object {
