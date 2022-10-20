8934a4ae9850d48665c0054110dd825fd7e4d2c9
jhuinee12 <jhuinee12@gmail.com>
2022-10-20 10:36:23 +0900

    [DEV-1-2] 당첨결과 화면 웹 크롤링

--- app/src/main/java/com/zahi/lotto/presentation/prize/PrizeViewModel.kt
+++ app/src/main/java/com/zahi/lotto/presentation/prize/PrizeViewModel.kt
@@ -1,20 +1,66 @@
 package com.zahi.lotto.presentation.prize
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.MutableLiveData
 import com.zahi.lotto.entity.LotteryNumber
 import com.zahi.lotto.repositories.WinnerRepository
 import com.zahi.lotto.base.BaseViewModel
+import com.zahi.lotto.entity.LotteryItem
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import org.jsoup.Jsoup
+import org.jsoup.select.Elements
 
 
 class PrizeViewModel(private val winnerRepository: WinnerRepository) : BaseViewModel() {
     var drwNo: MutableLiveData<Long> = MutableLiveData<Long>()
+    var lotteryItem: MutableLiveData<ArrayList<LotteryItem>> = MutableLiveData<ArrayList<LotteryItem>>()
     var lotteryData: MutableLiveData<LotteryNumber> = MutableLiveData<LotteryNumber>()
 
     fun getLottoWinnerNumber() {
         winnerRepository.subjectLotteryData.subscribe {
             lotteryData.value = it
+            doTask("https://www.dhlottery.co.kr/gameResult.do?method=byWin&drwNo=${drwNo.value}")
         }
         winnerRepository.getLottoWinnerNumber(drwNo.value!!)
     }
 
+
+    @SuppressLint("CheckResult")
+    private fun doTask(url : String) {
+        var documentTitle = ""
+        val itemList: ArrayList<LotteryItem> = arrayListOf() //MovieItem 배열
+
+        Single.fromCallable {
+            try {
+                val doc = Jsoup.connect(url).get()
+
+                val elements : Elements = doc.select("table tbody tr")
+                run elemLoop@{
+                    elements.forEach{
+                        val element = it.select("td")
+                        val rank = element[0].text()
+                        val sumPrize = element[1].text()
+                        val people = element[2].text()
+                        val prize = "￦ ${element[3].text().replace("원","")}"
+
+                        val item = LotteryItem(rank, sumPrize, people, prize)
+                        itemList.add(item)
+                    }
+                }
+
+                documentTitle = doc.title()
+            } catch (e : Exception) {e.printStackTrace()}
+
+            return@fromCallable documentTitle   // subscribe 호출
+        }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    lotteryItem.value = itemList
+                },
+                { it.printStackTrace() })
+    }
 }
